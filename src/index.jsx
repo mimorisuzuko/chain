3add72a9a4856d7ef3660f0e55a2a1ea8377f52b
mimorisuzuko <hrky0206@gmail.com>
2018-05-13 13:31:41 +0900

    Fixed #17

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import './index.scss';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 render(<App />, document.querySelector('main'));
